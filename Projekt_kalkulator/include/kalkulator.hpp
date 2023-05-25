f88cd4cf6b7c79c0172d74950a513513ad4ebe68
Tomasz Zydron <tzydron@student.agh.edu.pl>
2023-05-25 21:48:08 +0200

    utworzenie klasy Vect (wektory), konstruktory, operatory, iteratory

--- Projekt_kalkulator/include/kalkulator.hpp
+++ Projekt_kalkulator/include/kalkulator.hpp
@@ -3,8 +3,34 @@
 #define PROJEKT_KALKULATOR_KALKULATOR_HPP
 
 #include <vector>
+#include <cstddef>
+#include <string>
 
+//klasa odpoiwadająca za wektory
+class Vect {
+public:
+    //konstruktory
+    Vect(std::size_t n) : v_(n, 0) {}
+    Vect(const std::vector<int>& v) : v_(v) {}
 
+    //operatory
+    const int& operator[](std::size_t position) const {return v_[position];}
+    int& operator[](std::size_t position) {return v_[position];}
+
+    //iteratory
+    std::vector<int>::const_iterator cbegin() const {return v_.cbegin(); }
+    std::vector<int>::const_iterator cend() const {return v_.cend(); }
+
+    std::vector<int>::iterator begin() {return v_.begin(); }
+    std::vector<int>::iterator end() {return v_.end(); }
+
+    std::vector<int>::const_iterator begin() const {return v_.cbegin(); }
+    std::vector<int>::const_iterator end() const {return v_.cend(); }
+private:
+    std::vector<int> v_;
+};
+
+//klasa odpowiadająca za wielomiany
 class Polynomial{
 public:
     Polynomial(int size, float val = 0) : coeficient_(size, val) {}
