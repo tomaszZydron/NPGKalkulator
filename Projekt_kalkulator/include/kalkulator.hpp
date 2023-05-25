aac6ba87fd682279191f26cce1a40edf25c4a5e1
Mateusz Szwaja <mateuszszwaja03@gmail.com>
2023-05-25 21:38:58 +0200

    klasa wielomianów + 2 konstruktory + 2 operatory

--- Projekt_kalkulator/include/kalkulator.hpp
+++ Projekt_kalkulator/include/kalkulator.hpp
@@ -2,4 +2,16 @@
 #ifndef PROJEKT_KALKULATOR_KALKULATOR_HPP
 #define PROJEKT_KALKULATOR_KALKULATOR_HPP
 
+#include <vector>
+
+
+class Polynomial{
+public:
+    Polynomial(int size, float val = 0) : coeficient_(size, val) {}
+    Polynomial(const std::vector<float>& v) : coeficient_(v) {}
+    const float& operator[](std::size_t pos) const { return coeficient_[pos]; }
+    float& operator[](std::size_t pos) { return coeficient_[pos]; }
+private:
+    std::vector<float> coeficient_;
+};
 #endif //PROJEKT_KALKULATOR_KALKULATOR_HPP
