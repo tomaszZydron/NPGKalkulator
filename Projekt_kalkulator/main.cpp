d39133dfc256fa5e4e27be288d3a8115dd1860f8
Konrad Paszko <pppkondzio@gmail.com>
2023-05-27 19:52:21 +0200

    usprawnienie interfejsu uzytkownika

--- Projekt_kalkulator/main.cpp
+++ Projekt_kalkulator/main.cpp
@@ -2,5 +2,21 @@
 
 int main() {
     std::cout << "Witaj użytkowniku w kalkulatorze matematycznym  :P ";
+    std::cout << "Wtbierz na czym chcesz wykonać operacje \n 1 - wektory \n 2 - macierze \n 3 - wielomiany";
+    int odp;
+    std::cin >> odp;
+    if (odp == 1) {
+        std::cout
+                << "Wtbierz operacje jaka chcesz wykonać \n 1 -dodawanie \n 2 - iloczyn skalarny \n 3 - mnożenie przez skalar";
+        std::cin >> odp;
+    }
+    else if (odp == 2) {
+        std::cout << "";
+        std::cin >> odp;
+    }
+    else if (odp == 3) {
+        std::cout << "";
+        std::cin >> odp;
+    }
     return 0;
 }
