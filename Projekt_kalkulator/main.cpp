52de5b56caaad9df88f0ac051d850eee3c2e0ac5
Konrad Paszko <pppkondzio@gmail.com>
2023-05-28 21:06:49 +0200

    dalsze usprawnienie interfejsu uzytkownika

--- Projekt_kalkulator/main.cpp
+++ Projekt_kalkulator/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     std::cout << "Witaj użytkowniku w kalkulatorze matematycznym  :P ";
@@ -6,13 +7,63 @@ int main() {
     int odp;
     std::cin >> odp;
     if (odp == 1) {
-        std::cout
-                << "Wtbierz operacje jaka chcesz wykonać \n 1 -dodawanie \n 2 - iloczyn skalarny \n 3 - mnożenie przez skalar";
+        std::cout<< "Wtbierz operacje jaka chcesz wykonać \n 1 -dodawanie \n 2 - iloczyn skalarny \n 3 - mnożenie przez skalar";
         std::cin >> odp;
+        if (odp == 1){ //dodawanie
+            int rozmiar = 0;
+            while (rozmiar <= 1){
+                std::cout<< "Podaj rozmiar wektora: ";
+                std::cin >> rozmiar;
+            }
+            std::vector < int > v1;
+            std::cout << "wpisz pierwszy wektor";
+            int x;
+            for (int i = 0; i < rozmiar; i++){
+                std::cin>>x;
+                v1.push_back(x);
+            }
+            std::vector < int > v2;
+            std::cout << "wpisz drugi wektor";
+            for (int i = 0; i < rozmiar; i++){
+                std::cin>>x;
+                v2.push_back(x);
+            }
+            std:std::cout << "Suma wektorów to: " ;
+        }
+        if (odp == 2){ // iloczyn skalarny
+            int rozmiar = 0;
+            while (rozmiar <= 1){
+                std::cout<< "Podaj rozmiar wektora: ";
+                std::cin >> rozmiar;
+            }
+            std::vector < int > v1;
+            std::cout << "wpisz pierwszy wektor";
+            int x;
+            for (int i = 0; i < rozmiar; i++){
+                std::cin>>x;
+                v1.push_back(x);
+            }
+            std::vector < int > v2;
+            std::cout << "wpisz drugi wektor";
+            for (int i = 0; i < rozmiar; i++){
+                std::cin>>x;
+                v2.push_back(x);
+            }
+            std:std::cout << "Suma wektorów to: " ;
+        }
+        if (odp == 3){ // mnożenie przez skalar
+
+        }
     }
     else if (odp == 2) {
-        std::cout << "";
+        std::cout<< "Wtbierz operacje jaka chcesz wykonać \n 1 -dodawanie \n 2 - iloczyn";
         std::cin >> odp;
+        if (odp == 1){
+
+        }
+        if (odp == 2){
+
+        }
     }
     else if (odp == 3) {
         std::cout << "";
